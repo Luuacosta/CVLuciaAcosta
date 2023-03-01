2fb257cb183b98eb433b101cfcbe2b8a5fd39885
Luuacosta <124268137+Luuacosta@users.noreply.github.com>
2023-03-01 07:09:45 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").addEventListener("click", function() {
+    console.log("Contactar!");
+    document.getElementById("demo").innerHTML = "Su solicitud de contacto ha sido enviada exitosamente!";
+});
